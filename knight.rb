37dca087866aaf3b1f0908be5d72ae55da59588a
Laura <Laura.Stephens@utexas.edu>
2017-02-07 14:32:13 -0600

    complete Knight class

--- knight.rb
+++ knight.rb
@@ -1,92 +1,100 @@
 class Knight
-	attr_accessor :position, :parent
+	attr_accessor :position, :parent, :children
 
 	def initialize(position=[0,0], parent = nil)
 		@position = position
 		@parent = parent
-		#@children = possible_positions(position)
+		@children = possible_moves_for(position)
 	end
 
-	def valid_moves_for(current_pos)
-		#puts current_pos.inspect
+	#returns array of possible moves for position
+	#only used to create children of nodes
+	def possible_moves_for(position)
 		possible_moves = []
-		#valid = []
-		x = set[0]
-		y = set[1]
-		#puts "x: #{x}   y: #{y}"
-		possible_moves.push(
-			[x+2, y+1],
-			[x+2, y-1],
-			[x+1, y+2],
-			[x+1, y-2],
-			[x-2, y+1],
-			[x-2, y-1],
-			[x-1, y+2],
-			[x-1, y-2])
-		#puts possible_moves.inspect
-		valid = possible_moves.map do |move|
-			valid << move if valid_move?(move)
-		end
 
-		puts valid.inspect
+		x = position[0]
+		y = position[1]
+		possible_moves.push(
+			[x+1, y+2],
+			[x-2, y-1],
+			[x+2, y-1],
+			[x+2, y+1],
+			[x-2, y+1],
+			[x-1, y-2],
+			[x-1, y+2],
+			[x+1, y-2])
+
+		possible_moves
+	end
+
+	#returns array of valid nodes from children(possible_moves) at position
+	def valid_moves_for(position, possible_moves)
+		valid = []
+		parent_node = self
+
+		possible_moves.each do |new_position|
+			valid << Knight.new(new_position, parent_node) if valid_move?(new_position) == true
+		end
 		valid
 	end
 
+	#check whether move is on board
 	def valid_move?(new_position)
-		return true
-		#return true if (new_position[0] >=0 && new_position[0] <=7) && (new_position[1] >=0 && new_position[1] <=7)
+		if (new_position[0] >=0 && new_position[0] <=7) && 
+			(new_position[1] >=0 && new_position[1] <=7) 
+			return true
+		end
 		false
 	end
 
+	#returns array of the node path
+	def path(node)
+		arr = [node.position]
 
-	def knight_moves(start_pos, end_pos)
-		self.start_pos = start_pos
-		@start_pos = start_pos
-		final = []
-		final << start_pos
-		return final if start_pos == end_pos
-		array = valid_moves(self.start_pos)
-
-		while !array.empty?
-
-			if array.include?(end_pos)
-				puts "array includes end position from the getgo"
-				final << end_pos
-				return final
-			end
-
-			set = array.shift
-			puts "set: #{set}"
-			#final << set
-			if valid_moves(set).include?(end_pos) #|| array.includes?(end_pos)
-				#puts 2
-				while !set.parent.nil?
-					final << set.start_pos
-				end
-
-
-				#puts "valid: #{valid_moves(set).inspect}"
-				#final << set
-				final << end_pos
-				return final
-			else
-				#puts 3
-				#puts "valid: #{valid_moves(set).inspect}"
-				valid_moves(set).each {|m| array<<Knight.new(m, set)}
-				#final.pop
-			end
-		end	
-		#puts "final: #{final}"
+		while node.parent != nil
+			node = node.parent
+			arr.unshift(node.position)
+		end
+		arr
 	end
 
+	#returns path of node
+	def knight_moves(start_pos, end_pos)
+		self.position = start_pos
+		node = self
+
+		if node.position == end_pos
+			return path(node)
+		end
+
+		parent_node = self
+		nodes = valid_moves_for(start_pos, self.children)
+
+		until nodes.empty?
+			node = nodes.shift
+			if node.position == end_pos
+				return path(node)
+			else
+				parent_node = node
+				valid_moves_for(node.position, node.children).each do |n|
+					n.parent = parent_node
+					nodes << n
+				end
+			end
+		end
+	end
 
 end
 
 
 
-k = Knight.new
+
+# 	k = Knight.new
+
+# puts k.knight_moves([3,3],[3,3]).inspect
 # puts k.knight_moves([0,0],[1,2]).inspect
-#puts k.knight_moves([0,0],[3,3]).inspect
+# puts k.knight_moves([0,0],[3,3]).inspect
 # puts k.knight_moves([3,3],[0,0]).inspect
-puts k.knight_moves([3,3],[4,3]).inspect
+# puts k.knight_moves([3,3],[4,3]).inspect
+#k.knight_moves([3,3],[4,3])
 #=>[3,3], [4,5], [2,4], [4,3]
